f029e228e67054c3d9999ace359ef9c4e4d32a91
Colin <colindcirian@gmail.com>
2023-06-26 23:42:50 -0700

    added routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
 import React from 'react';
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home";
 
-function loadingPage() {
+function App() {
   return (
-    <div className="home">
-      <h1>Pokemon Card Finder</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
-loadingPage();
 
-export default loadingPage;
+export default App;
