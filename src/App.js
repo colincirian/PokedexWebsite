91e15a79d40bcdd7e9905f668dc6029ef2e329b0
Brandon <rbrandon087@gmail.com>
2023-06-27 22:00:28 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
+import pokeballVideo from './public/pokeball.mp4';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route src="" element={<Home />} />
     </Routes>
   );
 }
