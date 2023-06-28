d751026ff559dd814efb4d74398388d28b9c4db7
Brandon <rbrandon087@gmail.com>
2023-06-27 22:42:12 -0700

    added sets.js added starting code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
-import pokeballVideo from './public/pokeball.mp4';
 
 
 function App() {
