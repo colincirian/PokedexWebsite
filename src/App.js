6e981b8caba8e431b1b42e268ce68ef773120265
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-06-27 17:25:49 -0700

    added README.md

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="" alt="" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Waiting for something amazing....I guess
         </p>
         <a
           className="App-link"
@@ -15,7 +14,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
