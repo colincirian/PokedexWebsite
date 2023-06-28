90e0ea3f58391b52f2fec5a4e8a75b9a8316fdbd
Brandon <rbrandon087@gmail.com>
2023-06-27 22:50:42 -0700

    I have added a entry point  from sets.js to app.js

--- server/controllers/sets.js
+++ server/controllers/sets.js
@@ -1,1 +1,15 @@
 import React from 'react'
+
+function Sets() {
+    const cardSets = ['Set 1', 'Set 2', 'Set 3', 'Set 4']
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default Sets;
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Sets from '../server/controllers/sets';
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
 
