3d55aa17fded388d4e4783fb76bf20bf4ddbd2a5
Brandon <rbrandon087@gmail.com>
2023-06-28 20:24:29 -0700

    moved home.js to src and added to my sets.js

--- server/controllers/sets.js
+++ server/controllers/sets.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-function Sets() {
-    const cardSets = ['Set 1', 'Set 2', 'Set 3', 'Set 4']
+const Sets = () => {
+  const cardSets = [
+    { id: 1, name: 'Set 1', description: 'Description of Set 1' },
+    { id: 2, name: 'Set 2', description: 'Description of Set 2' },
+    { id: 3, name: 'Set 3', description: 'Description of Set 3' },
+    { id: 4, name: 'Set 4', description: 'Description of Set 4' },
+    { id: 5, name: 'Set 5', description: 'Description of Set 5' },
+    // card sets
+  ];
 
-
-    return (
-        <div>
-
+  return (
+    <div>
+      <h1>Sets</h1>
+      {cardSets.map((set) => (
+        <div key={set.id}>
+          <h2>{set.name}</h2>
+          <p>{set.description}</p>
         </div>
-    )
-}
-
+      ))}
+    </div>
+  );
+};
 
 export default Sets;
