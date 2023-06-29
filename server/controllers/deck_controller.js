fafcb651028fb4c98bcf8f8685b2053f6945d6ec
Colin <colindcirian@gmail.com>
2023-06-28 21:59:43 -0700

    more organization

--- server/controllers/deck_controller.js
+++ server/controllers/deck_controller.js
@@ -1,1 +1,5 @@
-const express = require('express').Router();
+// DEPENDENCIES
+const express = require('express')
+const baker = express.Router()
+const Baker = require('../models/baker.js')
+const bakerSeedData = require('../models/baker_seed.js')
