7e9a69c84ac4d06d9b38f992c620b891fc90b3e3
Colin <colindcirian@gmail.com>
2023-06-28 18:57:07 -0700

    MORE FILES

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      
+    </>
   );
 }
 
