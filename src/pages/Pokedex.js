256a68edee1fd0c58ce908fa4dfde79068af251a
Damon Herrera <bustinbubs@gmail.com>
2023-06-29 23:16:50 -0700

    pokedex

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,12 +1,45 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function Pokedex() {
-    return(
-        <div>
-            <h1>Hello this is the Pokedex</h1>
+    const [pokemonName, setPokemonName] = useState("")
+    const [pokemon, setPokemon] = useState({})
 
-        </div>
-    )
+    const searchPokemon = () => {
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+            (response) => {
+                setPokemonName({
+                    name: pokemonName, 
+                    species: response.data.species.name,
+                    img: response.data.sprites.front_default,
+                    hp: response.data.stats[0].base_stat,
+                    attack: response.data.stats[1].base_stat, 
+                    defense: response.data.stats[2].base_stat,
+                    type: response.data.types[0].type.name,
+                })
+            }
+        );
+    };
+
+
+
+return(
+    <div className="App">
+        <div className="TitleSection">
+            <h1>This is the Pokedex</h1>
+                <input type="text" onChange={(event) => 
+                    {setPokemonName(event.target.value);
+            }}
+            />
+            <button onClick={searchPokemon}>Search Pokemon</button>
+         </div>
+         <div>
+            
+         </div>
+    </div>
+);
+  
 }
 
 export default Pokedex;
