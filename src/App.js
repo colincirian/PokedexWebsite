1ec3df47ce048f3235f19efd79d3f06adbb223cb
Colin <colindcirian@gmail.com>
2023-06-29 22:00:17 -0700

    linked pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import Home from './Home';
+import Home from './pages/Home';
 import "bootstrap/dist/css/bootstrap.css";
+import Navbar from './pages/Navbar';
+import Pokedex from './pages/Pokedex';
+import Sets from './pages/Sets';
 
 function App() {
   return (
     <div className="App">
       <h1>My App</h1>
       <Home />
+      <Navbar />
+      <Pokedex />
+      <Sets />
     </div>
   );
 }
