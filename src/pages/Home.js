b158b868377f74cab864c277b974df82934e360b
Colin <colindcirian@gmail.com>
2023-07-01 14:43:00 -0700

    idk

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,18 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Home() {
-  const container = {
-    backgroundColor: "white",
-    color: "white",
-    fontSize: "25px",
-    width: "100%",
-    height: "100vh",
-    justifyContent: "center",
-    alignItems: "center",
-    display: "flex"
-  };
-
-  const box = {
-    backgroundColor: "blue",
-    width: "200px",
-    height: "350px",
-    margin: "1rem"
-  };
 
   return (
-    <div className="homepage" style={container}>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-      <div className="box" style={box}/>
-    </div>
-  )};
+    <body className="home-body">
+      <div className="box-container">
+        <div className="boxes"></div>
+      </div>
+    </body>
+
+
+  );
+}
 
 export default Home;
