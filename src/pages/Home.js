f00ee1421fece411e6c651a5581f925bf0c6de06
Colin <colindcirian@gmail.com>
2023-07-03 22:44:04 -0700

    updated code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import "../App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
-  const home = {
-    backgroundColor: 'black',
-    color: 'white',
-    fontSize: '25px'
-  };
-
-
   return (
-<<<<<<< HEAD
-    <body img src="./images/pikachu.jpg">
-      <div>
-
-      </div>
-    </body>
-
-  )};
-=======
-    <div className="homepage" style={home}>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
+    <div className="homepage">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarTogglerDemo03"
@@ -34,32 +18,32 @@ function Home() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <Link class="navbar-brand" to="/Home">Poke Website</Link>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 navbar-link">
-              <li class="nav-link">
-                <Link class="nav-link active" to="/">Home</Link>
+          <Link className="navbar-brand" to="/Home">Poke Website</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-link">
+              <li className="nav-link">
+                <Link className="nav-link active" to="/">Home</Link>
               </li>
-              <li class="nav-link">
-                <Link class="nav-link" to="/sets">Sets</Link>
+              <li className="nav-link">
+                <Link className="nav-link" to="/sets">Sets</Link>
               </li>
-              <li class="nav-link">
-              <Link class="nav-link" to="/Pokedex">Pokedex</Link>
+              <li className="nav-link">
+                <Link className="nav-link" to="/Pokedex">Pokedex</Link>
               </li>
-              <li class="nav-item">
-              <Link class="nav-link" to="/Login">Login</Link>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Login">Login</Link>
               </li>
             </ul>
-            <form class="d-flex" role="search">
+            <form className="d-flex" role="search">
               <input
-                class="form-control me-2 search-input"
+                className="form-control me-2 search-input"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button class="btn btn-outline-success search-button" type="submit">
+              <button className="btn btn-outline-success search-button" type="submit">
                 <FontAwesomeIcon icon={faSearch} />
               </button>
             </form>
@@ -69,6 +53,5 @@ function Home() {
     </div>
   );
 }
->>>>>>> 7c73a80d05956752d9da5d3f66c93e202a402814
 
 export default Home;
