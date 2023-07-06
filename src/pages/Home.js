6780b0e0881ace2d28f97eb5be6627e80f6bf0c2
Colin <colindcirian@gmail.com>
2023-07-05 23:12:52 -0700

    kms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,83 +1,66 @@
 import React from "react";
-import Navbar from "./Navbar";
 import "../App.css";
 
 function Home() {
-  const container = {
-    backgroundColor: "#0B1B3D",
-    width: "100%",
-    minHeight: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "20px",
-    boxSizing: "border-box",
-  };
-
-  const background = {
-    backgroundColor: "#0B1B3D",
-    minHeight: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "20px",
-    boxSizing: "border-box",
-  };
-
-  const cardStyle = {
-    padding: "20px",
-    margin: "10px",
-    borderRadius: "5px",
-    cursor: "pointer",
-  };
-
-  const cardHoverEffect = (event) => {
-    
-  }
-
-  const img = {
-    width: "400px",
-    height: "400px",
-    borderRadius: "2rem",
-    borderRadius: "2rem",
+  const box = {
+    width: "100px",
+    height: "100px",
+    backgroundColor: "blue",
   };
 
   return (
-    <div style={background}>
-      <div className="homepage" style={container}>
-        <div>
-          <h1>CATCH THOSE MFS</h1>
-          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
-            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-          </a>
-        </div>
-
-        <div>
-          <h1>starter pack</h1>
-          <a
-            href="https://www.tcgplayer.com/categories/trading-and-collectible-card-games/pokemon?utm_campaign=18098386707&utm_source=google&utm_medium=cpc&utm_content=&utm_term=&adgroupid=&gclid=CjwKCAjwqZSlBhBwEiwAfoZUIEfYtgxVMMYk_VMdTKlCHCdpeEB0IdXmmRLEJo1fp1wKErObJufoIBoC4M4QAvD_BwE"
-            style={cardStyle}
-          >
-            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-          </a>
-        </div>
-
-        <div>
-          <h1>CATCH THOSE MFS</h1>
-          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
-            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-          </a>
-        </div>
-
-        <div>
-          <h1>CATCH THOSE MFS</h1>
-          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
-            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-          </a>
-        </div>
+    <div className="root" style={{ backgroundColor: "black", width: "100%", minHeight: "100vh" }}>
+      <div className="nav">
+        Navbar
       </div>
+
+      <main id="main" className="main">
+        <div id="home" style={box}>first link</div>
+        <div id="card-website-link" style={box}></div>
+        <div id="other-link" style={box}></div>
+      </main>
     </div>
   );
 }
 
 export default Home;
+
+
+//     <div>
+//       <Navbar />
+//       <div className="homepage" style={container}>
+//         <div>
+//             <h1 style={h1}>CATCH THOSE MFS</h1>
+//             <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
+//               <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+//             </a>
+//           </div>
+
+//           <div>
+//             <h1>starter pack</h1>
+//             <a
+//               href="https://www.tcgplayer.com/categories/trading-and-collectible-card-games/pokemon?utm_campaign=18098386707&utm_source=google&utm_medium=cpc&utm_content=&utm_term=&adgroupid=&gclid=CjwKCAjwqZSlBhBwEiwAfoZUIEfYtgxVMMYk_VMdTKlCHCdpeEB0IdXmmRLEJo1fp1wKErObJufoIBoC4M4QAvD_BwE"
+//               style={cardStyle}
+//             >
+//               <img src="./images/cards.jpg" alt="pokemon-go" style={img} />
+//             </a>
+//           </div>
+
+//         <div>
+//           <h1>CATCH THOSE MFS</h1>
+//           <a
+//             href="https://www.bmwusa.com/vehicles/m-models/m3-sedan/build-your-own.html"
+//             style={cardStyle}
+//           >
+//             <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+//           </a>
+//           <h1>CATCH THOSE MFS</h1>
+//           <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
+//             <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+//           </a>
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
