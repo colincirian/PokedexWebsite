36477339d11a8a3f29779cd15a1adee1b416035a
Colin <colindcirian@gmail.com>
2023-07-05 19:30:41 -0700

    pushing test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "../App.css";
-//import Navbar from "./Navbar";
-//import { Link } from "react-router-dom";
 
 function Home() {
   const container = {
     backgroundColor: "#0B1B3D",
     width: "100%",
+    minHeight: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "20px",
+    boxSizing: "border-box",
+  };
+
+  const background = {
+    backgroundColor: "#0B1B3D",
     minHeight: "100vh",
     display: "flex",
     alignItems: "center",
@@ -22,50 +30,49 @@ function Home() {
     cursor: "pointer",
   };
 
+  const cardHoverEffect = (event) => {
+    
+  }
+
   const img = {
     width: "400px",
     height: "400px",
-    borderRadius: "2rem"
+    borderRadius: "2rem",
   };
 
   return (
-    <div className="" style={container}>
-      <div>
-        <h1>CATCH THOSE MFS</h1>
-        <a
-          href="https://pokemongolive.com/?hl=en"
-          style={cardStyle}
-        >
-          <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-        </a>
-      </div>
+    <div style={background}>
+      <div className="homepage" style={container}>
+        <div>
+          <h1>CATCH THOSE MFS</h1>
+          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
+            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+          </a>
+        </div>
 
-      <div>
-        <h1>starter pack</h1>
-        <a
-          href="https://pokemongolive.com/?hl=en"
-          style={cardStyle}
-        >
-          <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-        </a>
-      </div>
-      
-      <div>
-        <h1>CATCH THOSE MFS</h1>
-        <a
-          href="https://pokemongolive.com/?hl=en"
-          style={cardStyle}
-        >
-          <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
-        </a>
-      </div>
-      
-      <div className="card" id="pokemon-go-link" style={cardStyle}>
-        <h1>hello</h1>
-        <p>
-          The leap into electronic typesetting, remaining essentially unchanged.
-          It was popularized in the 1960s with the release of Letraset.
-        </p>
+        <div>
+          <h1>starter pack</h1>
+          <a
+            href="https://www.tcgplayer.com/categories/trading-and-collectible-card-games/pokemon?utm_campaign=18098386707&utm_source=google&utm_medium=cpc&utm_content=&utm_term=&adgroupid=&gclid=CjwKCAjwqZSlBhBwEiwAfoZUIEfYtgxVMMYk_VMdTKlCHCdpeEB0IdXmmRLEJo1fp1wKErObJufoIBoC4M4QAvD_BwE"
+            style={cardStyle}
+          >
+            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+          </a>
+        </div>
+
+        <div>
+          <h1>CATCH THOSE MFS</h1>
+          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
+            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+          </a>
+        </div>
+
+        <div>
+          <h1>CATCH THOSE MFS</h1>
+          <a href="https://pokemongolive.com/?hl=en" style={cardStyle}>
+            <img src="./images/pokemonGo.jpg" alt="pokemon-go" style={img} />
+          </a>
+        </div>
       </div>
     </div>
   );
