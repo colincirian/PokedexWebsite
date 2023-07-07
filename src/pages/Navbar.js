a9dac41ae985e89a1492295f4935c00a05b1ee57
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-07-06 17:57:05 -0700

    added log out feature to navbar.js

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import "../App.css";
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import supabase from '../Services/supabaseClient';
 
 const Navbar = () => {
-    const handleSearchSubmit = (event) => {
-        event.preventDefault();
+
+    const handleLogout = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.log("Error logging out:", error.message);
+        } else {
+            window.alert("You have successfully logged out!");
+        }
     }
 
     return (
@@ -25,11 +30,8 @@ const Navbar = () => {
                     >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    {/* Navbar brand */}
                     <Link className="navbar-brand" to="/">Poke Website</Link>
-                    {/* Navbar collapsible content */}
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                        {/* Navbar links */}
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 navbar-link">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
@@ -46,19 +48,10 @@ const Navbar = () => {
                             <li className="nav-item">
                                 <Link className="nav-link" to="/SignUp">Sign Up</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" onClick={handleLogout}>Logout</Link>
+                            </li>
                         </ul>
-                        {/* Search form */}
-                        <form className="d-flex" role="search" onSubmit={handleSearchSubmit}>
-                            <input
-                                className="form-control me-2 search-input"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button className="btn btn-outline-success search-button" type="submit">
-                                <FontAwesomeIcon icon={faSearch} />
-                            </button>
-                        </form>
                     </div>
                 </div>
             </nav>
