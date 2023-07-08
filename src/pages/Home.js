e9a05f5a3bf6cd45a827510e38a4c32b84ede705
Colin <colindcirian@gmail.com>
2023-07-08 10:39:40 -0700

    home css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,14 @@ import "../App.css";
 
 function Home() {
 
-  const anchorClickEvent = () => {
-    console.log("element clicked!");
+  const hoverEffect = (event) => {
+    event.target.style.backgroundColor = "#FFA500";
+    event.target.style.transform = "translateY(-15px)";
+  };
+
+  const hoverEffectLeave = (event) => {
+    event.target.style.backgroundColor = "transparent";
+    event.target.style.transform = "translateY(0)";
   };
 
   const screen = {
@@ -15,18 +21,20 @@ function Home() {
   };
 
   const boxes = {
-    width: "400px",
-    height: "400px",
+    width: "380px",
+    height: "100px",
     borderRadius: "1rem",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "transparent",
-    border: "solid white 4px",
+    border: "solid black 4px",
     margin: "1rem",
     color: "white",
     fontSize: "20px",
-    cursor: "pointer"
+    cursor: "pointer",
+    marginTop: "20px",
+    transition: "transform 0.4s ease"
   };
 
   const main_container = {
@@ -49,7 +57,7 @@ function Home() {
     color: "white",
     fontSize: "30px",
     color: "black",
-    position: "relative"
+    position: "abolute"
   };
 
   const title = {
@@ -64,23 +72,43 @@ function Home() {
         <div style={background_image}>
           <div id="main" style={main_container}>
 
-            <div id="card-website-link" style={boxes}>
+            <div 
+              id="card-website-link" 
+              style={boxes}
+              onMouseEnter={hoverEffect} // CALL HOVER EFFECT
+              onMouseLeave={hoverEffectLeave} //  CALL HOVER EFFECT LEAVE
+              >
               <Link style={anchor} to="/Pokedex">POKEDEX</Link>
             </div>
 
-            <div id="home" style={boxes}>
+            <div 
+              id="home" 
+              style={boxes}
+              onMouseEnter={hoverEffect} // CALL HOVER EFFECT
+              onMouseLeave={hoverEffectLeave} //  CALL HOVER EFFECT LEAVE
+              >
               <a style={anchor} href="https://pokemongolive.com/?hl=en">
                 POKEMON GO
               </a>
             </div>
 
-            <div id="other-link" style={boxes}>
+            <div 
+              id="other-link" 
+              style={boxes}
+              onMouseEnter={hoverEffect} // CALL HOVER EFFECT
+              onMouseLeave={hoverEffectLeave} //  CALL HOVER EFFECT LEAVE
+              >
               <a style={anchor} href="https://www.pokemon.com/us/pokemon-news">
                 LATEST POKEMON NEWS
                 </a>
             </div>
 
-            <div id="other-link" style={boxes}>
+            <div 
+              id="other-link" 
+              style={boxes}
+              onMouseEnter={hoverEffect} // CALL HOVER EFFECT
+              onMouseLeave={hoverEffectLeave} //  CALL HOVER EFFECT LEAVE
+              >
               <a style={anchor} href="https://www.tcgplayer.com/categories/trading-and-collectible-card-games/pokemon?utm_campaign=18098386707&utm_source=google&utm_medium=cpc&utm_content=&utm_term=&adgroupid=&gclid=CjwKCAjwqZSlBhBwEiwAfoZUIEfYtgxVMMYk_VMdTKlCHCdpeEB0IdXmmRLEJo1fp1wKErObJufoIBoC4M4QAvD_BwE">
                 PURCHASE CARDS
                 </a>
