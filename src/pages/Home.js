facfec162dddfa01849e446c7469410739d58881
Colin <colindcirian@gmail.com>
2023-07-07 20:44:24 -0700

    COMING ALONG

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function Home() {
@@ -10,14 +11,12 @@ function Home() {
   const screen = {
     width: "100vw",
     minHeight: "100vh",
-    backgroundColor: "#020036",
-    margin: "auto",
-    padding: "0",
+    backgroundColor: "#FFA500"
   };
 
   const boxes = {
-    width: "200px",
-    height: "50px",
+    width: "400px",
+    height: "400px",
     borderRadius: "1rem",
     display: "flex",
     justifyContent: "center",
@@ -31,46 +30,63 @@ function Home() {
   };
 
   const main_container = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100%",
-    margin: "0"
+    display: "flex"
   };
 
   const background_image = {
     backgroundImage: "url('./images/pikachu.jpg",
-    width: "90%",
-    height: "90vh",
     justifyContent: "center",
     alignItems: "center",
+    width: "90vw",
+    height: "100vh",
     margin: "auto",
-    display: "flex"
+    display: "flex",
+    borderRadius: "1.5rem"
   }
 
   const anchor = {
     textDecoration: "none",
     color: "white",
+    fontSize: "30px",
+    color: "black",
+    position: "relative"
   };
-  
+
+  const title = {
+    position: "absolute",
+    margin: "auto",
+    fontSize: "50px"
+  };
 
   return (
       <div style={screen}>
+        <h1 style={title}>POKEMON CARD WEBSITE</h1>
         <div style={background_image}>
-          <main id="main" style={main_container}>
-                <div id="home" style={boxes}>
-                  <a style={anchor} href="https://pokemongolive.com/?hl=en">Pokedex</a>
-                  </div>
+          <div id="main" style={main_container}>
 
-                <div id="card-website-link" style={boxes}>Card Sets</div>
+            <div id="card-website-link" style={boxes}>
+              <Link style={anchor} to="/Pokedex">POKEDEX</Link>
+            </div>
 
-                <div id="other-link" style={boxes}>
-                  <a style={anchor} href=""></a>
-                </div>
+            <div id="home" style={boxes}>
+              <a style={anchor} href="https://pokemongolive.com/?hl=en">
+                POKEMON GO
+              </a>
+            </div>
 
-                <div id="other-link" style={boxes}>hello</div>
-          </main>
+            <div id="other-link" style={boxes}>
+              <a style={anchor} href="https://www.pokemon.com/us/pokemon-news">
+                LATEST POKEMON NEWS
+                </a>
+            </div>
+
+            <div id="other-link" style={boxes}>
+              <a style={anchor} href="https://www.tcgplayer.com/categories/trading-and-collectible-card-games/pokemon?utm_campaign=18098386707&utm_source=google&utm_medium=cpc&utm_content=&utm_term=&adgroupid=&gclid=CjwKCAjwqZSlBhBwEiwAfoZUIEfYtgxVMMYk_VMdTKlCHCdpeEB0IdXmmRLEJo1fp1wKErObJufoIBoC4M4QAvD_BwE">
+                PURCHASE CARDS
+                </a>
+              </div>
+
+          </div>
         </div>
       </div>
   );
