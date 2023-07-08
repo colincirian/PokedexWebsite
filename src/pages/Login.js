f2c220cb111923b91993daea4299c686d67583b9
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-07-08 12:41:36 -0700

    supabaseClient.js file moved and added to gitignore

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,48 +2,46 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-import supabase from "../Services/supabaseClient";
-
+import supabase from "./Services/supabaseClient";
 const Login = () => {
-  const [state, setState] = useState({ email: "", password: "" });
-  const navigate = useNavigate();
+ const [state, setState] = useState({ email: "", password: "" });
+ const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    try {
-      const { email, password } = state;
-  
-      // Validate the input
-      if (!email || !password) {
-        // Display an error message or handle the validation error
-        return;
-      }
+ const handleLogin = async () => {
+  try {
+   const { email, password } = state;
 
-      // console.log("Form Data:", { email, password }); // Log the form data
+   // Validate the input
+   if (!email || !password) {
+    // Display an error message or handle the validation error
+    return;
+   }
 
-  
-      // Sign in with Supabase
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      });
-  
-      if (error) {
-        console.error("Login failed:", error.message);
-        alert("Login failed! Please check your credentials and try again.");
-        return;
-      }
-  
-      // Authentication successful
-      // Store the token in local storage or a browser cookie
-      localStorage.setItem("token", data.access_token);
-  
-      // Redirect the user to the desired page or update the UI accordingly
-      navigate("/");
-    } catch (error) {
-      // Handle network errors or other exceptions
-      console.error("Login error:", error);
-    }
-  };
+   // console.log("Form Data:", { email, password }); // Log the form data
+
+   // Sign in with Supabase
+   const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password,
+   });
+
+   if (error) {
+    console.error("Login failed:", error.message);
+    alert("Login failed! Please check your credentials and try again.");
+    return;
+   }
+
+   // Authentication successful
+   // Store the token in local storage or a browser cookie
+   localStorage.setItem("token", data.access_token);
+
+   // Redirect the user to the desired page or update the UI accordingly
+   navigate("/");
+  } catch (error) {
+   // Handle network errors or other exceptions
+   console.error("Login error:", error);
+  }
+ };
 
  const loginContainerStyle = {
   display: "flex",
